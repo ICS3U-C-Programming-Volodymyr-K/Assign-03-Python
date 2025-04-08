8b226985fd846862606a645ee50ba2026e531840
Volodymyr Kryzhanovskyi <volodymyr.kryzhnovskyi@stu.ocsb.ca>
2025-04-08 17:09:58 +0000

    Updated for the code.

--- assign_03.py
+++ assign_03.py
@@ -14,22 +14,28 @@ def main():
         b_int = float(b_str)
         c_int = float(c_str)
         question_1 = input(
-            "Is your quadratic equation in power of something? If it is, type yes, if it is not then type no")
+        "Is your quadratic equation in power of something? If it is, type yes, if it is not then type no")
         if question_1.lower() == "yes":
                 power = input("Enter to what power will your quadratic equation be.")
                 try:
                     power =float(power)
                     print(f"Your formula looks like, ({a_int}**{power}**{power} + {b_int}**{power}x**{power} + {c_int}**{power})")
                 except ValueError:
-                    print("Enter integer.")
+                    print("Enter the float.")
         elif question_1.lower() == "no":
             print(f"Your formula looks like, {a_int}x**2 + {b_int}x + {c_int}")
             print("We will use the discriminant theory to find the roots of equation.")
             discriminant = b_int**2 -4*a_int*c_int
-            x_one = (-b_int + math.sqrt(discriminant))/2*a_int
-            x_two = (-b_int - math.sqrt(discriminant))/2*a_int
-            print(f"The roots of quadratic problem are {x_one} and {x_two}")
+            try:
+                discriminant >= 0
+                x_one = (-b_int + math.sqrt(discriminant))/2*a_int
+                x_two = (-b_int - math.sqrt(discriminant))/2*a_int
+                print(f"The roots of quadratic problem are {x_one} and {x_two}")
+            except Exception:
+                print("Discriminant can't be negative.")
+        question_2 = input("Do you want to calculate the parabola in vertex formula? Types yes if you want and no if you don't want.")
+        if question_2.lower() == "yes."
     except ValueError:
-         print("Integer needed,")
+        print("Correct input needed, look through comments or code to determine what variable you need.")
 if __name__ == "__main__":
     main()
