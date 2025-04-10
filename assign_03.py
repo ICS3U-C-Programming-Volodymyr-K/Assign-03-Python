753758ced443e3d7025dd4167ae9d172466f717a
Volodymyr Kryzhanovskyi <volodymyr.kryzhnovskyi@stu.ocsb.ca>
2025-04-10 03:26:15 +0000

    I made huge adjustments for the code, added lots of stuff for it and updated old ones.

--- assign_03.py
+++ assign_03.py
@@ -13,71 +13,6 @@ def main():
         a_int = float(a_str)
         b_int = float(b_str)
         c_int = float(c_str)
-        question_1 = input(
-            "Is your quadratic equation in power of something? If it is, type yes, if it is not then type no")
-        if question_1.lower() == "yes":
-            power = input(
-                "Enter to what power will your quadratic equation be.")
-            try:
-                power = float(power)
-                a_int**(power) + b_int**(power) + c_int**(power)
-                x_power = 2**(power)
-                print(
-                    f"The formula will look like,{a_int**(power)}x**{x_power} + {b_int**(power)}x**{power} + {c_int**(power)}")
-                a_int = a_int ** (1/power)
-                b_int = b_int ** (1/power)
-                c_int = c_int ** (1/power)
-                print(
-                    f"The simplified formula will look like {power}√ over each number {a_int}x**2 + {b_int}x + {c_int}")
-                print(
-                    "We will use the discriminant theory to find the roots of equation.")
-                discriminant = b_int**2 - 4*a_int*c_int
-                try:
-                    discriminant >= 0
-                    if discriminant > 0:
-                        if discriminant == 0:
-                            if b_int < 0:
-                                a_int = math.sqrt(a_int)
-                                c_int = math.sqrt(c_int)
-                                print(
-                                    f"Your quadratic equation will look like, ({a_int} - {c_int})**2")
-                            elif b_int > 0:
-                                a_int = math.sqrt(a_int)
-                                c_int = math.sqrt(c_int)
-                                print(
-                                    f"Your quadratic equation will look like, ({a_int}+{b_int})**2")
-                        x_one = (-b_int + math.sqrt(discriminant))/2*a_int
-                        x_two = (-b_int - math.sqrt(discriminant))/2*a_int
-                        print(
-                            f"The roots of quadratic problem are {x_one} and {x_two}")
-                except Exception:
-                    print("Discriminant can't be negative.")
-            except ValueError:
-                print("Enter the float.")
-        elif question_1.lower() == "no":
-            print(f"Your formula looks like, {a_int}x**2 + {b_int}x + {c_int}")
-            print("We will use the discriminant theory to find the roots of equation.")
-            discriminant = b_int**2 - 4*a_int*c_int
-            try:
-                discriminant >= 0
-                if discriminant > 0:
-                    if discriminant == 0:
-                        if b_int < 0:
-                            a_int = math.sqrt(a_int)
-                            c_int = math.sqrt(c_int)
-                            print(
-                                f"Your quadratic equation will look like, ({a_int} - {c_int})**2")
-                        elif b_int > 0:
-                            a_int = math.sqrt(a_int)
-                            c_int = math.sqrt(c_int)
-                            print(
-                                f"Your quadratic equation will look like, ({a_int}+{b_int})**2")
-                    x_one = (-b_int + math.sqrt(discriminant))/2*a_int
-                    x_two = (-b_int - math.sqrt(discriminant))/2*a_int
-                    print(
-                        f"The roots of quadratic problem are {x_one} and {x_two}")
-            except Exception:
-                print("Discriminant can't be negative.")
         question_2 = input(
             "Do you want to calculate the parabola in vertex formula? Types yes if you want and no if you don't want.")
         if question_2 == "yes":
@@ -91,7 +26,73 @@ def main():
             elif b_int > 0:
                 c_int = a_int*-e_int + c_int
             print(
-                f"Your formula in vertex looks like {a_int} (x + {math.sqrt(d_int)}) + {c_int}")
+                f"Your formula in vertex looks like {a_int}(x + {math.sqrt(d_int)}) {c_int}")
+        elif question_2.lower() == "no":
+            question_1 = input(
+            "Is your quadratic equation in power of something? If it is, type yes, if it is not then type no")
+            if question_1.lower() == "yes":
+                power = input(
+                "Enter to what power will your quadratic equation be.")
+                try:
+                    power = float(power)
+                    a_int**(power) + b_int**(power) + c_int**(power)
+                    x_power = 2**(power)
+                    print(
+                        f"The formula will look like,{a_int**(power)}x**{x_power} + {b_int**(power)}x**{power} + {c_int**(power)}")
+                    a_int = a_int ** (1/power)
+                    b_int = b_int ** (1/power)
+                    c_int = c_int ** (1/power)
+                    print(
+                        f"The simplified formula will look like {power}√ over each number {a_int}x**2 + {b_int}x + {c_int}")
+                    print(
+                        "We will use the discriminant theory to find the roots of equation.")
+                    discriminant = b_int**2 - 4*a_int*c_int
+                    try:
+                        discriminant >= 0
+                        if discriminant > 0:
+                            if discriminant == 0:
+                                if b_int < 0:
+                                    a_int = math.sqrt(a_int)
+                                    c_int = math.sqrt(c_int)
+                                    print(
+                                        f"Your quadratic equation will look like, ({a_int} - {c_int})**2")
+                                elif b_int > 0:
+                                    a_int = math.sqrt(a_int)
+                                    c_int = math.sqrt(c_int)
+                                    print(
+                                        f"Your quadratic equation will look like, ({a_int}+{b_int})**2")
+                            x_one = (-b_int + math.sqrt(discriminant))/2*a_int
+                            x_two = (-b_int - math.sqrt(discriminant))/2*a_int
+                            print(
+                                f"The roots of quadratic problem are {x_one} and {x_two}")
+                    except Exception:
+                        print("Discriminant can't be negative.")
+                except ValueError:
+                    print("Enter the float.")
+            elif question_1.lower() == "no":
+                print(f"Your formula looks like, {a_int}x**2 + {b_int}x + {c_int}")
+                print("We will use the discriminant theory to find the roots of equation.")
+                discriminant = b_int**2 - 4*a_int*c_int
+                try:
+                    discriminant >= 0
+                    if discriminant > 0:
+                        if discriminant == 0:
+                            if b_int < 0:
+                                a_int = math.sqrt(a_int)
+                                c_int = math.sqrt(c_int)
+                                print(
+                                f"Your quadratic equation will look like, ({a_int} - {c_int})**2")
+                            elif b_int > 0:
+                                a_int = math.sqrt(a_int)
+                                c_int = math.sqrt(c_int)
+                                print(
+                                    f"Your quadratic equation will look like, ({a_int}+{b_int})**2")
+                    x_one = (-b_int + math.sqrt(discriminant))/2*a_int
+                    x_two = (-b_int - math.sqrt(discriminant))/2*a_int
+                    print(
+                        f"The roots of quadratic problem are {x_one} and {x_two}")
+                except Exception:
+                    print("Discriminant can't be negative.")
     except ValueError:
         print("Correct input needed, look through comments or code to determine what variable you need.")
 if __name__ == "__main__":
